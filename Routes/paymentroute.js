66e2403aa8c557f8efcb993e7db12a17335fdfa8
sridhar0005 <sridhar0005@gmail.com>
2020-11-30 10:20:09 +0530

    new

--- Routes/paymentroute.js
+++ Routes/paymentroute.js
@@ -1,10 +1,95 @@
 
-let express = require('express'),
-router = express.Router();
+const express = require('express');
+const router = express.Router();
+const https = require('https')
+const qs = require('querystring')
+const bodyParser = require('body-parser');
+
+
+const checksum_lib = require('../Paytm/checksum')
+const config = require('../Paytm/config')
+
+// Middleware for body parsing
+const parseUrl = bodyParser.urlencoded({ extended: false })
+const parseJson = bodyParser.json({ extended: false })
 
 const paymentController= require('../controllers/paymentcontroller');
 
-router.get('/getOrderid',paymentController.getOrderConfirm);
+router.get('/getorderconfirm',[parseUrl, parseJson],paymentController.getOrderConfirm);
+
+router.post('/getorderDetails',[parseUrl, parseJson],paymentController.getorderDetails);
+
+
+router.post('/paynow', [parseUrl, parseJson],  paymentController.payNow );
+
+router.post('/callback', (req, res) => {
+    console.log('hello', req.body)
+    var body = '';
+  
+    req.on('data', function (data) {
+        console.log(data)
+        body += data;
+    });
+  
+    req.on('end', function () {
+      var html = "";
+      var post_data = qs.parse(body);
+  
+      // received params in callback
+      console.log('Callback Response: ', post_data, "\n");
+  
+  
+      // verify the checksum
+      var checksumhash = post_data.CHECKSUMHASH;
+      // delete post_data.CHECKSUMHASH;
+      var result = checksum_lib.verifychecksum(post_data, config.PaytmConfig.key, checksumhash);
+      console.log("Checksum Result => ", result, "\n");
+  
+  
+      // Send Server-to-Server request to verify Order Status
+      var params = { "MID": config.PaytmConfig.mid, "ORDERID": post_data.ORDERID };
+  
+      checksum_lib.genchecksum(params, config.PaytmConfig.key, function (err, checksum) {
+  
+        params.CHECKSUMHASH = checksum;
+        post_data = 'JsonData=' + JSON.stringify(params);
+  
+        var options = {
+          hostname: 'securegw-stage.paytm.in', // for staging
+          // hostname: 'securegw.paytm.in', // for production
+          port: 443,
+          path: '/merchant-status/getTxnStatus',
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': post_data.length
+          }
+        };
+  
+  
+        // Set up the request
+        var response = "";
+        var post_req = https.request(options, function (post_res) {
+          post_res.on('data', function (chunk) {
+            response += chunk;
+          });
+  
+          post_res.on('end', function () {
+            console.log('S2S Response: ', response, "\n");
+  
+            var _result = JSON.parse(response);
+            res.render('response', {
+              'data': _result
+            })
+          });
+        });
+  
+        // post the data
+        post_req.write(post_data);
+        post_req.end();
+      });
+    });
+  })
 
 
 module.exports = router;
--- controllers/paymentcontroller.js
+++ controllers/paymentcontroller.js
@@ -1,6 +1,11 @@
 
 var mysql = require('mysql');
 
+const request = require('request');
+
+const checksum_lib = require('../Paytm/checksum')
+const config = require('../Paytm/config')
+
 
 var db1 = mysql.createConnection({
     host: 'localhost',
@@ -36,8 +41,8 @@ module.exports.getOrderConfirm = async function (req, res) {
 
     } else {
 
-        const query1 = "SELECT `smsportal_authkey` FROM `app_clients_master` WHERE `agent_id`='10831aa4'"
-        await db2.query(query1, function (err, response, fields) {
+        const query1 = "SELECT `smsportal_authkey` FROM `app_clients_master` WHERE `agent_id` =?"
+        await db1.query(query1,[agent_id], function (err, response, fields) {
             if (!response.length) {
                 res.status(400).send({
                     status: "error",
@@ -48,7 +53,7 @@ module.exports.getOrderConfirm = async function (req, res) {
                 if (authkey === smsportal_authkey) {
                     res.send({
                         status: "success",
-                        paygateway_url:'http://payment.nutanapp.in'
+                        paygateway_url:'http://payment.nutanapp.in/'+order_id
 
                     });
                 }
@@ -63,3 +68,72 @@ module.exports.getOrderConfirm = async function (req, res) {
     }
 
 }
+
+
+
+module.exports.getorderDetails = async (req,res) => {
+
+    const orderIdApi='https://portalapi.nutansms.in/fetchOrderdetailsV1.php';
+ 
+ 
+    const options = {
+      url: orderIdApi,
+      qs: { order_id: 'autosms/'+req.body.order_id},
+      headers: {
+        'Authorization': 'nh7bhg5f*c#fd@sm9'
+      },
+    
+      json: true,
+      method: 'Post',
+    }
+    console.log(options)
+    request(options, (err, response, body) => {
+      // console.log(err)
+      // console.log(response)
+    //   console.log(body)
+
+      if (err) {
+        res.json(err)
+      } else {
+        res.json(body)
+
+      }
+    });
+  
+
+
+}
+
+module.exports.payNow = (req, res) => {
+    if (!req.body.amount || !req.body.email || !req.body.phone) {
+        res.status(400).send('Payment failed')
+      } else {
+        var params = {};
+        params['MID'] = config.PaytmConfig.mid;
+        params['WEBSITE'] = config.PaytmConfig.website;
+        params['CHANNEL_ID'] = 'WEB';
+        params['INDUSTRY_TYPE_ID'] = 'Retail';
+        params['ORDER_ID'] = 'TEST_' + new Date().getTime();
+        params['CUST_ID'] = 'customer_001';
+        params['TXN_AMOUNT'] = req.body.amount.toString();
+        params['CALLBACK_URL'] = 'http://localhost:3008/callback';
+        params['EMAIL'] = req.body.email;
+        params['MOBILE_NO'] = req.body.phone.toString();
+    
+        checksum_lib.genchecksum(params, config.PaytmConfig.key, function (err, checksum) {
+          var txn_url = "https://securegw-stage.paytm.in/theia/processTransaction"; // for staging
+          // var txn_url = "https://securegw.paytm.in/theia/processTransaction"; // for production
+    
+          var form_fields = "";
+          for (var x in params) {
+            form_fields += "<input type='hidden' name='" + x + "' value='" + params[x] + "' >";
+          }
+          form_fields += "<input type='hidden' name='CHECKSUMHASH' value='" + checksum + "' >";
+    
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.write('<html><head><title>Merchant Checkout Page</title></head><body><center><h1>Please do not refresh this page...</h1></center><form method="post" action="' + txn_url + '" name="f1">' + form_fields + '</form><script type="text/javascript">document.f1.submit();</script></body></html>');
+          res.end();
+        });
+      }
+}
+
